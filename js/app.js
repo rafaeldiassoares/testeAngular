20d2303dec5727cdae276508ac7dacb70c942bf2
rafaeldiassoares <rafael_gtt@hotmail.com>
2015-03-23 17:41:53 -0300

    teste angular

---
+++ js/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var app = angular.module('sysApp', ['ngRoute', 'ngResource', 'directives']);
+
+app.config(function($routeProvider){
+    $routeProvider
+        .when('/', {        
+            controller: 'InicioCtrl',
+            templateUrl: '/templates/inicio.html'
+        })
+        .when('/dashboard', {        
+            controller: 'DashboardCtrl',
+            templateUrl: '/templates/dashboard.html'
+        })
+        .when('/charts', {        
+            controller: 'ChartsCtrl',
+            templateUrl: '/templates/charts.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        }) ;
+});
---
+++ js/controllers/chartsCtrl.js
@@ -0,0 +1,3 @@
+app.controller('ChartsCtrl', function($scope){
+
+});
